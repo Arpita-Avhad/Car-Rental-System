4776f7a9bd5d77f151a92b6bba4096c071f2fca8
Arpita-Avhad <145573120+Arpita-Avhad@users.noreply.github.com>
2024-02-08 23:22:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -244,24 +244,24 @@ class CarRentalSystem {
     public static void main(String[] args) {
         CarRentalSystem rentalSystem = new CarRentalSystem();
 
-        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
+        Car car1 = new Car("C001", "Toyota", "Camry", 60.0);      // Different base price per day for each car
         Car car2 = new Car("C002", "Honda", "Accord", 70.0);
         Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
-		Car car4 = new Car("C004", "Honda", "Creta", 120.0);
+	Car car4 = new Car("C004", "Honda", "Creta", 120.0);
         Car car5 = new Car("C005", "Tata", "Punch", 80.0);
         Car car6 = new Car("C006", "Mahindra", "Scorpio", 170.0);
-		Car car7 = new Car("C007", "Tata", "Nexon", 140.0);
+	Car car7 = new Car("C007", "Tata", "Nexon", 140.0);
         Car car8 = new Car("C008", "kia", "Sonet", 100.0);
 
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
-		rentalSystem.addCar(car4);
+	rentalSystem.addCar(car4);
         rentalSystem.addCar(car5);
         rentalSystem.addCar(car6);
-		rentalSystem.addCar(car7);
+	rentalSystem.addCar(car7);
         rentalSystem.addCar(car8);
-
+	    
         rentalSystem.menu();
     }
 }
